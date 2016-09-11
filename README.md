87ba78e177652ddbdb800a6121a2a05e247e3ee0
Tom Chantler <tom@chantler.net>
2016-09-12 00:27:27 +0100

    First working version
    This is the version I'm running in production

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Ghost-Azure-BackupDb
-Backup your SQLite database when running Ghost in Azure Web Apps
+
+**Backup your SQLite database with zero downtime when running Ghost in Azure Web Apps**
+
+If you're hosting your Ghost blog in Azure then you're probably not backing up your database, even if you think you are. All of the solutions I found online required me either to export the blog data manually (as a json file via the Ghost admin panel), or to stop my blog from running. This very simple solution does away with that requirement.
+
+You can either build the code yourself or simply download `CompiledWebJob\Ghost-Azure-BackupDb.zip` and upload it to your Azure Web App as a WebJob.
+
+Full instructions here: https://tomssl.com/2016/09/12/backup-your-sqlite-database-with-zero-downtime-when-running-ghost-in-azure-web-apps
---
+++ src/Ghost-Azure-BackupDb/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+
+namespace Ghost_Azure_BackupDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"d:\home\site\wwwroot\content\data";
+            var dbPath = $"{path}{Path.DirectorySeparatorChar}ghost.db";
+            var dbBackupPath = $"{path}{Path.DirectorySeparatorChar}backup.db";
+            Console.WriteLine($"Backup running in {System.AppContext.BaseDirectory}");
+            Console.WriteLine($"About to backup db from {dbPath} to {dbBackupPath}");
+            using (var source = new SQLiteConnection($"Data Source={dbPath}; Version=3;"))
+            using (var destination = new SQLiteConnection($"Data Source={dbBackupPath}; Version=3;"))
+            {
+                source.Open();
+                destination.Open();
+                source.BackupDatabase(destination, "main", "main", -1, null, 0);
+            }
+            Console.WriteLine("Backed up db successfully");
+        }
+    }
+}
